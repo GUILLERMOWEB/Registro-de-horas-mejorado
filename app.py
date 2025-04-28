8fbc5069ea6713c57dcd698bdd7efc72e2535dff
Guillermo <ghgenet7@gmail.com>
2025-04-28 07:31:12 -0300

    Correcion cargar cliente, agrego dependencias nuevas

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ from flask_migrate import Migrate
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField
+
 
 #from app import db
 
