8cb55294ab4c4648a2204125aef9fdc968c49742
Guillermo <ghgenet7@gmail.com>
2025-04-28 13:26:47 -0300

    Revision models

--- models.py
+++ models.py
@@ -7,21 +7,6 @@ db = SQLAlchemy()
 
 # -----------------------------
 # MODELO DE USUARIOS
-# -----------------------------
-class User(db.Model, UserMixin):
-    __tablename__ = 'users'
-
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(150), nullable=False, unique=True)
-    password = db.Column(db.String(150), nullable=False)
-    role = db.Column(db.String(50), nullable=False, default='user')  # user, admin, superadmin
-    email = db.Column(db.String(150), nullable=True)
-
-    registros = db.relationship('RegistroHoras', backref='user', lazy=True)
-
-    def __repr__(self):
-        return f'<User {self.username}>'
-
 
 # -----------------------------
 # MODELO DE REGISTRO DE HORAS
